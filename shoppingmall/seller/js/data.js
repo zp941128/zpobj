286c632e780bead63d55fbf3541ad0f41a9fbe2a
zp941128 <550253560@qq.com>
2018-11-17 11:43:09 +0800

    three

---
+++ shoppingmall/seller/js/data.js
@@ -0,0 +1,90 @@
+var fund=
+    {
+        "balance":'145.00',
+        "market":'123.00',
+        "receipted":'1000.00',
+        "unreceipted":'2000.00'
+    }
+
+var article=[
+    {
+       "src":'img_seller/1496210830044.jpg',
+        "name":'小米手机p9',
+        "style":'红色，16G',
+        "type":'爆款手机',
+        "value":'1999',
+        "inventory":'200',
+        "all":'13',
+        "date":'2018-9-1'
+    },
+    {
+        
+                "src":'img_seller/1496210830044.jpg',
+                "name":'苹果手机',
+                "style":'红色，16G',
+                "type":'爆款手机',
+         
+        "value":'8999',
+        "inventory":'200',
+        "all":'13',
+        "date":'2018-9-1'
+    }
+]
+
+var indet=[
+    {   
+        "worksheet":'001',
+        "merchant":'朱鹏店铺02',
+        "buyerphone":'12345678901',
+        "commodity":[
+            {
+                "src":"img_seller/1496210830044.jpg",
+                "commname":'小米手机',
+                "type":'国产好手机',
+                "style":'红色，16G',
+                "value":'1999'
+            },
+            {
+                "src":"img_seller/1496210830044.jpg",
+                "commname":'苹果手机',
+                "type":'高档手机',
+                "style":'红色，16G',
+                "value":'1999'
+            }
+        ],
+        "buyer":'小王'
+    },
+    {
+        "worksheet":'001',
+        "merchant":'朱鹏店铺01',
+        "buyerphone":'12345678901',
+        "commodity":[
+            {
+                "src":"img_seller/1496210830044.jpg",
+                "commname":'三枪内裤',
+                "type":'世界名牌内裤',
+                "style":'红色超大',
+                "value":'9999'
+            }
+            
+        ],
+        "buyer":'小狗'
+    }
+]
+
+var deal=[
+    {
+        "numberical":'15131127',
+        "commname":'苹果10x',
+        "style":'红，512G',
+        "value":'12000',
+        "state":'已经交易'
+    },
+    {
+        "numberical":'12345567',
+        "commname":'华为mata 20 pro',
+        "style":'紫，512G',
+        "value":'14000',
+        "state":'交易失败'
+    }
+]
